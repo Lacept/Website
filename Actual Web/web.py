d95d43024830398e56ed501396eb8f3de7d32a11
Lolepopie <8401103+lolepop@users.noreply.github.com>
2020-01-16 02:18:02 +0800

    some comments

--- Actual Web/web.py
+++ Actual Web/web.py
@@ -3,4 +3,11 @@ app = Flask(__name__) #initializes the Flask object
 @app.route('/<path:filename>') #associates the root() function with the ‘/’ route
 def root(filename):
     return send_from_directory("templates", filename) #serve up the ‘index.html’ webpage
+
+@app.route("/api/calcScore")
+def calcScore():
+    # find how to get the form body here
+    # return json {"art": [], "science" : []}
+    pass
+
 app.run(debug=True) #run the app, this must correspond to the variable name you chose
