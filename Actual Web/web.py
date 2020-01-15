3619e3b83c270b819e9d49cd4234a81d70b4c532
Lolepopie <8401103+lolepop@users.noreply.github.com>
2020-01-15 20:30:37 +0800

    untested stuff and bugfix

--- Actual Web/web.py
+++ Actual Web/web.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template #import from the flask library the functions Flask and render_template
+from flask import Flask, send_from_directory #import from the flask library the functions Flask and render_template
 app = Flask(__name__) #initializes the Flask object
-@app.route('/') #associates the root() function with the ‘/’ route
-def root():
-    return render_template('main_page.html') #serve up the ‘index.html’ webpage
+@app.route('/<path:filename>') #associates the root() function with the ‘/’ route
+def root(filename):
+    return send_from_directory("templates", filename) #serve up the ‘index.html’ webpage
 app.run(debug=True) #run the app, this must correspond to the variable name you chose
